c7fc9410fc3ded7192aa8a7b49e0e18912075699
yutanchen <31685877+yutanchen@users.noreply.github.com>
2023-11-05 17:10:27 +0800

    Update makehtml.py

--- makehtml.py
+++ makehtml.py
@@ -1,12 +1,23 @@
-from pandas import read_excel                                                                                                                
+from pandas import read_excel                                                                                                                             
 from datetime import date
-from sys import argv 
-doctors=['主治醫師',]
-titles=['','']
-mywidth=[]
+from sys import argv
+
+# fill
+doctors=['授課教師','王名儒']
+titles=['A','B','C','D','E','F','G','H','I','J','K']
+mywidth={'A':50,'B':100,'C':30,'D':30,'E':30,'F':30,'G':30,'H':30,'I':30,'J':30,'K':50}
+
 #df = read_excel("temp.xlsx", names=NL, usecols="A:K")
-df = read_excel(argv[1], names=titles, usecols="")
+df = read_excel(argv[1], names=titles, usecols="A:K")
+f=open('out.html',encoding='utf-8',mode ='w')
+f.write('<!DOCTYPE html><html><body style="margin 0px 0px;">\n')
+f.write('<h1 style="text-align:center;">Patient List ('+str(date.today())+')</h1>\n')
+f.write('<table style="font-size:8px;">\n')
 
 for i, row in df.iterrows():
-  if row['doctor'] in doctors:
-    print(row[''], row[''])
+    if row['K'] in doctors:
+        f.write('<tr>')
+        for j in ['A','B','K']:f.write('<td width=' + str(mywidth[j]) + '>' + str(row[j]) +'</td>')
+        f.write('<br></tr>\n')
+f.write('</table></body></html>')
+
